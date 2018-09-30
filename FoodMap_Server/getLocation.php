bce4a53ded8e8257702fb5185241aa71aa297cec
Châu Hoàng Phúc <1612520@student.hcmus.vn>
2018-09-30 21:26:33 +0700

    added getLocation

---
+++ FoodMap_Server/getLocation.php
@@ -0,0 +1,34 @@
+<?php 
+	//import library
+	include "../private/database.php"
+	
+	$id_rest = $_POST['id_rest'];
+	//create class Comment
+	class Comment{
+		function Comment($date_time, $id_rest, $guest_email, $owner_email){
+			$this->date_time = $date_time;
+			$this->id_rest = $id_rest;
+			$this->guest_email = $guest_email;
+			$this->owner_email = $owner_email;
+		}
+	}
+	
+	//create query string
+	$query = "SELECT * FROM COMMENTS WHERE ID_REST = " . $id_rest . " ORDER BY DATE_TIME";
+	
+	//create connection
+	$conn = new database();
+	//connect
+	$conn->connect();
+	//get result
+	$listComments = $conn->query($query);
+	$response = array();
+	foreach ($listComments as $row) {
+		array_push($response, new Comment($row['date_time'], $row['id_rest'], $row['guest_email'], $row['owner_email']));
+	}
+	
+	//close conn
+	$conn->disconnect();
+	//response
+	echo json_encode($response);
+?>
