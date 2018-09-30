b4f9b7a89b655f090f8f72d121cbe46907310568
Châu Hoàng Phúc <1612520@student.hcmus.vn>
2018-09-30 14:34:31 +0700

    update getDiner

--- FoodMap_Server/getDiner.php
+++ FoodMap_Server/getDiner.php
@@ -4,19 +4,19 @@
 	include "../private/database.php"
 	
 	//create class Restaurant
-	class Restaurant{
-		function Restaurant($id, $id_user, $name, $address, $phone_number, $describe_text, $url_image, $time_open, $time_close){
-			$this->id = $id;
-			$this->id_user = $id_user;
-			$this->name = $name;
-			$this->address = $address;
-			$this->phone_number = $phone_number;
-			$this->describe_text = $describe_text;
-			$this->url_image = $url_image;
-			$this->time_open = $time_open;
-			$this->time_close = $time_close;
-		}
-	}
+	//class Restaurant{
+	//	function Restaurant($id, $id_user, $name, $address, $phone_number, $describe_text, $url_image, $time_open, $time_close){
+	//		$this->id = $id;
+	//		$this->id_user = $id_user;
+	//		$this->name = $name;
+	//		$this->address = $address;
+	//		$this->phone_number = $phone_number;
+	//		$this->describe_text = $describe_text;
+	//		$this->url_image = $url_image;
+	//		$this->time_open = $time_open;
+	//		$this->time_close = $time_close;
+	//	}
+	//}
 	
 	//create query string
 	$query = "SELECT * FROM RESTAURANT";
