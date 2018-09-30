52360b83ec13bbaefcf025b9d9b7bcda2f72face
Châu Hoàng Phúc <1612520@student.hcmus.vn>
2018-09-30 14:58:55 +0700

    update getDiner

--- FoodMap_Server/getDiner.php
+++ FoodMap_Server/getDiner.php
@@ -4,19 +4,19 @@
 	include "../private/database.php"
 	
 	//create class Restaurant
-	//class Restaurant{
-	//	function Restaurant($id, $id_user, $name, $address, $phone_number, $describe_text, $url_image, $time_open, $time_close){
-	//		$this->id = $id;
-	//		$this->id_user = $id_user;
-	//		$this->name = $name;
-	//		$this->address = $address;
-	//		$this->phone_number = $phone_number;
-	//		$this->describe_text = $describe_text;
-	//		$this->url_image = $url_image;
-	//		$this->time_open = $time_open;
-	//		$this->time_close = $time_close;
-	//	}
-	//}
+	class Restaurant{
+		function Restaurant($id, $id_user, $name, $address, $phone_number, $describe_text, $url_image, $time_open, $time_close){
+			$this->id = $id;
+			$this->id_user = $id_user;
+			$this->name = $name;
+			$this->address = $address;
+			$this->phone_number = $phone_number;
+			$this->describe_text = $describe_text;
+			$this->url_image = $url_image;
+			$this->time_open = $time_open;
+			$this->time_close = $time_close;
+		}
+	}
 	
 	//create query string
 	$query = "SELECT * FROM RESTAURANT";
@@ -27,9 +27,14 @@
 	$conn->connect();
 	//get result
 	$listRestaurants = $conn->query($query);
+	$response = array();
+	foreach ($listRestaurants as $row) {
+		array_push($response, new Restaurant($row['id'], $row['id_user'], $row['name'], $row['address'], $row['phone_number'], $row['describe_text'], $row['url_image'], $row['time_open'], $row['time_close']));
+	}
+	
 	
 	//close conn
 	$conn->disconnect();
 	//response
-	echo json_encode($listRestaurants);
+	echo json_encode($response);
 ?>
