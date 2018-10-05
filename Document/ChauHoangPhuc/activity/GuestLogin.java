b6157609a46d7a401a750eb9b4b887ab3c8d8abb
Châu Hoàng Phúc <1612520@student.hcmus.vn>
2018-10-05 21:31:57 +0700

    added guestactivity

---
+++ Document/ChauHoangPhuc/activity/GuestLogin.java
@@ -0,0 +1,16 @@
+package com.study.android.foodmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+public class GuestLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_login);
+    }
+}
